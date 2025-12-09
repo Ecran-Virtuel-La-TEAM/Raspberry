8000deeb8ab4fba2453698ea60deb2b676b3d8d2
Eliott <git.survivor856@passmail.net>
2025-12-09 15:23:52 +0100

    ADC test 2

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 from serial import Serial
 
+# Settings
+RECORDING_TIME = 10 # s
+FILE = "/home/groupe11/Documents/Raspberry/data/A0_data.bin"
+
 BAUDS = 921600  # Same value as in the Arduino : Serial.begin()
-
-buffer = bytearray()
 BUFFER_SIZE = 1000
-file_size_kb = 0
-MAX_FILE_SIZE_KB = 5 * 1024 # 5 MB
 
-with Serial("/dev/serial0", BAUDS, timeout=1) as serial, open("A0_data.bin", "wb") as file:
+# Constants
+buffer = bytearray()
+file_size_kb = 0
+
+SAMPLING_FREQUENCY = 8000
+MAX_FILE_SIZE_KB = round(RECORDING_TIME * SAMPLING_FREQUENCY / BUFFER_SIZE) 
+
+print(f"file {FILE} will take {MAX_FILE_SIZE_KB} kB, do you want to continue ?", end=" ")
+if input() not in ("y", "yes", ""):
+    exit("Bye")
+
+with Serial("/dev/ttyACM0", BAUDS, timeout=5) as serial, open(FILE, "wb") as file:
     while True:
         a0, a1, a2, a3 = serial.read(4)
 
